f5b018fc87746757cf5b5d223cd879ad475dd7ee
Alyssa Harris <ah00214@my.westga.edu>
2021-06-09 18:57:23 -0400

    Created bst class and added Node and show functions

---
+++ bst.js
@@ -0,0 +1,13 @@
+module.exports.Node = Node;
+module.exports.BST = BST;
+
+function Node(data, left, right) {
+  this.data = data;
+  this.left = left;
+  this.right = right;
+  this.show = show;
+}
+
+function show() {
+  return this.data;
+}
