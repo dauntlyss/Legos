b58261f579c37482d21d5b63643f32c235b1a32b
Alyssa Harris <ah00214@my.westga.edu>
2021-06-09 19:16:57 -0400

    Tested insert root function

---
+++ test/testBSTInsert.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+var lego = require('../legos');
+var bst = require('../bst');
+
+const Brick = lego.Brick;
+const BST = bst.BST;
+
+describe('testBST', function() {
+  describe('root test', function() {
+    it('assert brick0 is inserted as root node', function() {
+      var brick0 = new Brick(1, "red");
+      var tree = new BST();
+      tree.insert(brick0);
+      assert.equal(tree.root.data, brick0);
+    });
+  });
+});
