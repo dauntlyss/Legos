d596c8308fbfae698508fdc01dd7d1fd7c43cecb
Alyssa Harris <ah00214@my.westga.edu>
2021-06-09 19:05:12 -0400

    Created bst insert function

--- bst.js
+++ bst.js
@@ -11,3 +11,37 @@ function Node(data, left, right) {
 function show() {
   return this.data;
 }
+
+function BST() {
+  this.root = null;
+  this.insert = insert;
+  this.inOrder = inOrder;
+}
+
+function insert(data) {
+  var n = new Node(data, null, null);
+  if (this.root == null) {
+    this.root = n;
+  }
+  else {
+    var current = this.root;
+    var parent;
+    while (true) {
+      parent = current;
+      if (data.size < current.data) {
+        current = current.left;
+        if (current == null) {
+          parent.left = n;
+          break;
+        }
+      }
+      else {
+        current = current.right;
+        if (current == null) {
+          parent.right = n;
+          break;
+        }
+      }
+    }
+  }
+}
