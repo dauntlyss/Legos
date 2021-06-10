a15dbefbc7d08f1d77d90c550990966d276c8f0d
Alyssa Harris <ah00214@my.westga.edu>
2021-06-09 20:00:01 -0400

    Tested root with right-most-child, and branching behaviors

--- test/testBSTInsert.js
+++ test/testBSTInsert.js
@@ -60,6 +60,25 @@ describe('testBST', function() {
       assert.equal(tree.root.left.data, brick1);
     });
 
+    it('assert four bricks with sizes of 10, 20, 30 and 40 inserted into tree will result in 30 as the root and 10 as the left most child', function() {
+      var brick0 = new Brick(41, "pink");
+      var brick1 = new Brick(50, "orange");
+      var brick2 = new Brick(27, "lavender");
+      var brick3 = new Brick(36, "yellow");
+      var brick4 = new Brick(45, "black");
+      var brick5 = new Brick(77, "blue");
+      var tree = new BST();
+      tree.insert(brick0);
+      tree.insert(brick1);
+      tree.insert(brick2);
+      tree.insert(brick3);
+      tree.insert(brick4);
+      tree.insert(brick5);
+      assert.equal(tree.root.left.data, brick2);
+      assert.equal(tree.root.right.left.data, brick4);
+      assert.equal(tree.root.right.right.data, brick5);
+    });
+
 
   });
 });
