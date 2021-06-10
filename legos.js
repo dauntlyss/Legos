581886a3ee306e0b848e8e65aa44ec59b023b1c6
Alyssa Harris <ah00214@my.westga.edu>
2021-06-09 20:07:53 -0400

    Created LegoPile function

--- legos.js
+++ legos.js
@@ -1,6 +1,22 @@
 module.exports.Brick = Brick;
+module.exports.LegoPile = LegoPile;
 
 function Brick(size, color) {
   this.size = size;
   this.color = color;
+}
+
+function LegoPile() {
+  this.datastore = {
+    "red" : new BST(),
+    "green" : new BST(),
+    "blue" : new BST(),
+    "yellow" : new BST(),
+    "black" : new BST(),
+    "white" : new BST()
+  };
+};
+
+function insert(Brick) {
+  this.datastore[Brick.color].insert(Brick)
 }
